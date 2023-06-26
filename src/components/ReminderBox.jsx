8a041fe7c7f6c38f3a79c984d58a24114191893e
Adeel Waqar <zxadeel@gmail.com>
2023-06-26 13:52:21 -0400

    updating remote

--- src/components/ReminderBox.jsx
+++ src/components/ReminderBox.jsx
@@ -1,22 +1,43 @@
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { ScrollView, View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { actions, RichEditor, RichToolbar } from "react-native-pell-rich-editor";
 
 
 export default function ReminderBox(){
+    const RichText = useRef();
     const navigation = useNavigation();
+    const [reminderData, setReminderData] = useState("")
 
+    useFocusEffect(
+        useCallback(() => {
+        async function fetchData() {
+            const items = await AsyncStorage.getItem('reminder');
+            if (items != null){
+                setReminderData(items)
+                console.log(reminderData)
+            }
+        }
+        fetchData();
+    }, [])
+    )
 
 
     return (
-        
-        <View style={styles.hadithContainer}>
-            <TouchableOpacity onPress={() => navigation.navigate("Reminders")}>
-            <ScrollView style={{paddingTop:10}}>
-                <Text style={{textAlign:'center',fontStyle:'italic'}}>This is just a placeholder</Text>
-            </ScrollView>
+        <TouchableOpacity onPress={() => navigation.navigate("Reminders")} style={styles.hadithContainer}>
+                <RichEditor
+                disabled={true}
+                ref={RichText}
+                placeholder={"Tap here to set reminders!"}
+                androidLayerType="software"
+                initialContentHTML={reminderData}
+                onChange={(text) => setReminderData(text)}
+                editorStyle= {styles.hadithContainer}
+                useContainer={false}
+                />
         </TouchableOpacity>
-
-        </View>
     )
 
 
@@ -39,4 +60,5 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         backgroundColor: "#FFEBCD",
     },
+
 });
